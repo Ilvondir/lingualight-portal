bef510d157d0f084f3f3e978384f000a0db32ffc
Ilvondir <komsa.m@o2.pl>
2023-05-19 14:29:29 +0200

    confirmations for trainers table added

---
+++ database/migrations/2023_05_19_121124_create_confirmations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('confirmations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("trainer_id");
+            $table->string("subject");
+            $table->text("message");
+            $table->string("date");
+            $table->string("file");
+            $table->foreign("trainer_id")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table("confirmations", function ($table) {
+            $table->dropForeign('confirmations_trainer_id_foreign');
+        });
+
+        Schema::dropIfExists('confirmations');
+    }
+};
